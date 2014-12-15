a2418268ae454ce7629656e33697a06de51dd292
davidgong89 <gongtuo@gmail.com>
2014-12-15 21:09:53 +0800

    revamp

--- www/js/app.js
+++ www/js/app.js
@@ -1,124 +1,140 @@
-// Ionic Starter App
+var app = angular.module('ionicApp', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'pytrackerv2' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('pytrackerv2', ['ionic'])
-
-/**
- * The Projects factory handles saving and loading projects
- * from local storage, and also lets us save and load the
- * last active project index.
- */
-.factory('Projects', function() {
-  return {
-    all: function() {
-      var projectString = window.localStorage['projects'];
-      if(projectString) {
-        return angular.fromJson(projectString);
-      }
-      return [];
-    },
-    save: function(projects) {
-      window.localStorage['projects'] = angular.toJson(projects);
-    },
-    newProject: function(projectTitle) {
-      // Add a new project
-      return {
-        title: projectTitle,
-        tasks: []
-      };
-    },
-    getLastActiveIndex: function() {
-      return parseInt(window.localStorage['lastActiveProject']) || 0;
-    },
-    setLastActiveIndex: function(index) {
-      window.localStorage['lastActiveProject'] = index;
-    }
-  }
-})
-
-.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
-
-  // A utility function for creating a new project
-  // with the given projectTitle
-  var createProject = function(projectTitle) {
-    var newProject = Projects.newProject(projectTitle);
-    $scope.projects.push(newProject);
-    Projects.save($scope.projects);
-    $scope.selectProject(newProject, $scope.projects.length-1);
-  }
-
-
-  // Load or initialize projects
-  $scope.projects = Projects.all();
-
-  // Grab the last active, or the first project
-  $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
-
-  // Called to create a new project
-  $scope.newProject = function() {
-    var projectTitle = prompt('Project name');
-    if(projectTitle) {
-      createProject(projectTitle);
-    }
-  };
-
-  // Called to select the given project
-  $scope.selectProject = function(project, index) {
-    $scope.activeProject = project;
-    Projects.setLastActiveIndex(index);
-    $ionicSideMenuDelegate.toggleLeft(false);
-  };
-
-  // Create our modal
-  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
-    $scope.taskModal = modal;
-  }, {
-    scope: $scope
-  });
-
-  $scope.createTask = function(task) {
-    if(!$scope.activeProject || !task) {
-      return;
-    }
-    $scope.activeProject.tasks.push({
-      title: task.title
-    });
-    $scope.taskModal.hide();
-
-    // Inefficient, but save all the projects
-    Projects.save($scope.projects);
-
-    task.title = "";
-  };
-
-  $scope.newTask = function() {
-    $scope.taskModal.show();
-  };
-
-  $scope.closeNewTask = function() {
-    $scope.taskModal.hide();
-  }
-
-  $scope.toggleProjects = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-
-
-  // Try to create the first project, make sure to defer
-  // this by using $timeout so everything is initialized
-  // properly
-  $timeout(function() {
-    if($scope.projects.length == 0) {
-      while(true) {
-        var projectTitle = prompt('Your first project title:');
-        if(projectTitle) {
-          createProject(projectTitle);
-          break;
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/login')
+	
+  $stateProvider	
+	.state('login', {
+			url:"/login",
+			templateUrl:"templates/login.html",
+			controller:"loginCtrl"
+	})
+	.state('forgot-password', {
+			url:"/forgot-password",
+			templateUrl:"templates/forgot-password.html",
+	})
+    .state('yoga-app', {
+	  //this is the parent list, cannot link here (treat as header)	, only link to child
+      url: "/yoga-app",
+	  //abstract enable nesting (certain parts of the page retained)
+      abstract: true,
+      templateUrl: "templates/yoga-app.html"
+    })
+    .state('yoga-app.profile', {
+      url: '/profile',
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/profile.html",
         }
       }
-    }
-  });
+    })
+    .state('yoga-app.classes', {
+      url: '/classes',
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/classes.html",
+        }
+      }
+    })
+	.state('yoga-app.workshops', {
+      url: "/workshops",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/workshops.html",
+        }
+      }
+    })
+	.state('yoga-app.events', {
+      url: "/events",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/events.html",
+        }
+      }
+    })
+	.state('yoga-app.appointments', {
+      url: "/appointments",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/appointments.html",
+        }
+      }
+    })
+	.state('yoga-app.challenges', {
+      url: "/challenges",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/challenges.html",
+        }
+      }
+    })
+	.state('yoga-app.posetree', {
+      url: "/posetree",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/posetree.html",
+        }
+      }
+    })
+	.state('yoga-app.luckydraw', {
+      url: "/luckydraw",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/luckydraw.html",
+        }
+      }
+    })
+	.state('yoga-app.halloffame', {
+      url: "/halloffame",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/halloffame.html",
+        }
+      }
+    })
+})
 
-});
+
+app.controller('loginCtrl', function($scope, $state) {
+	$scope.signIn = function(user) {
+		console.log('login', user);
+		$state.go('yoga-app.profile');
+	};
+})
+
+//$ionicSideMenuDelegate is the dependency for menu
+app.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+	//left menu
+	$scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+})
+
+app.controller('classesCtrl', function($scope, $ionicLoading, $timeout) {
+  $scope.classesData = {
+    "filter" : 'beginner',
+    "classes": [
+      {
+        type : "beginner",
+        name : "beginner class",
+		date : "12-12-14",
+		time : "12:30pm",
+		instructor: "Mrs Yuna"
+      },
+      {
+        type : "intermediate",
+        name : "intermediate class",
+		date : "12-12-14",
+		time : "12:30pm",
+		instructor: "Mrs Yuka"
+      },
+      {
+        type : "advance",
+        name : "advance class",
+		date : "12-12-14",
+		time : "12:30pm",
+		instructor: "Mrs Yuka"
+      }
+    ]
+  };
+})
